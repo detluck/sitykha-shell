f11cdaf82d0e25aad72646cbcce723b2ba40d5db
detluck <pashundel1987@gmail.com>
2026-06-01 20:38:36 +0200

    finished ConfigManeger.cpp

--- src/core/config/ConfigManager.cpp
+++ src/core/config/ConfigManager.cpp
@@ -1,6 +1,34 @@
 #include "ConfigManager.hpp"
+#include "ConfigObject.hpp"
+#include "LockConfig.hpp"
+#include "qdebug.h"
 #include "qdir.h"
 #include "qfileinfo.h"
+#include <qdebug.h>
+#include <qfiledevice.h>
+#include <qjsondocument.h>
+#include <qjsonobject.h>
+#include <qjsonparseerror.h>
+#include <qvariant.h>
+
+namespace sitykha::config {
+Q_LOGGING_CATEGORY(lcConfig, "sitykha.config")
+static Config *s_instance = nullptr;
+
+Config *Config::instance() {
+  if (!s_instance) {
+    s_instance = new Config();
+  }
+  return s_instance;
+}
+
+Config *Config::create(QQmlEngine *, QJSEngine *) { return instance(); }
+
+Config::Config(QObject *parent) : ConfigObject(parent) {
+  m_lock = new LockConfig(this);
+
+  connect(m_lock, &ConfigObject::modified, this, &Config::save);
+}
 
 void sitykha::config::Config::setup(const QString &path) {
   m_filePath = path;
@@ -41,3 +69,106 @@ void sitykha::config::Config::setup(const QString &path) {
 
   this->reload();
 }
+
+void Config::save() {
+  if (!m_loading) {
+    m_saveTimer->start();
+  }
+}
+
+void Config::reload() {
+  m_parseRetries = 0;
+  m_saveTimer->stop();
+  executeReload();
+}
+
+void Config::executeReload() {
+  if (m_filePath.isEmpty())
+    return;
+
+  QFile file(m_filePath);
+  if (!file.exists())
+    return;
+
+  if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    emit errorOccurred("Failed to read config: " + file.errorString());
+    qDebug(lcConfig) << "Failed to read config: " << file.errorString();
+    return;
+  }
+
+  QByteArray data = file.readAll();
+  if (data.isEmpty()) {
+    if (m_parseRetries < 3) {
+      m_parseRetries++;
+      m_retryTimer->start();
+      return;
+    }
+    emit errorOccurred("Config is empty after multipe retries");
+    qDebug(lcConfig) << "Config is empty";
+    return;
+  }
+
+  QJsonParseError error;
+  QJsonDocument doc = QJsonDocument::fromJson(data, &error);
+
+  if (error.error != QJsonParseError::NoError) {
+    if (m_parseRetries < 3) {
+      m_parseRetries++;
+      m_retryTimer->start();
+      return;
+    }
+    emit errorOccurred("Json parse error: " + error.errorString());
+    qDebug(lcConfig) << "Json parse error: " << error.errorString();
+    return;
+  }
+
+  m_loading = true;
+  this->loadJson(doc.object());
+  m_loading = false;
+
+  m_parseRetries = 0;
+  emit reloaded();
+}
+
+void Config::executeSave() {
+  if (m_filePath.isEmpty() || m_loading)
+    return;
+
+  QFileInfo fileInfo(m_filePath);
+  QDir().mkpath(fileInfo.absolutePath());
+
+  QJsonObject obj = this->saveToJson();
+  QJsonDocument doc(obj);
+
+  QFile file(m_filePath);
+  if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+    emit errorOccurred("Failed to write config: " + file.errorString());
+    qDebug(lcConfig) << "Failed to write config: " << file.errorString();
+    return;
+  }
+
+  m_recentlySaved = true;
+
+  file.write(doc.toJson(QJsonDocument::Indented));
+  file.close();
+
+  updateFileWatch();
+
+  emit saved();
+  m_cooldownTimer->start();
+}
+
+void Config::updateFileWatch() {
+  QStringList dirs = m_watcher->directories();
+  if (!dirs.contains(m_watchedDir)) {
+    m_watcher->addPath(m_watchedDir);
+  }
+}
+
+void Config::onWatcherEvent() {
+  if (m_recentlySaved)
+    return;
+  m_reloadTimer->start();
+}
+
+} // namespace sitykha::config
--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,8 +1,26 @@
+#include <QDebug>
+#include <QDir>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "config/ConfigManager.hpp"
+
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
+
+  QString configPath = QDir::homePath() + "/.config/sitykha/shell.json";
+  qDebug() << "Initializing Sitykha Config at:" << configPath;
+
+  auto *config = sitykha::config::Config::instance();
+  config->setup(configPath);
+
+  config->save();
+
   QQmlApplicationEngine engine;
+
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+      []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+
   return app.exec();
 }
